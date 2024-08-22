91deee59232495fa0e184d0c23c4e8295f6bd731
JerrickHO <108850086+JerrickHO@users.noreply.github.com>
2024-08-21 17:41:51 -0700

    Update server.js

--- server.js
+++ server.js
@@ -79,7 +79,3 @@ app.get('/bundles/:date', async (req, res) => {
 app.listen(PORT, () => {
   console.log('Server is running on http://localhost:'+ PORT);
 });
-// Start the server
-app.listen(PORT, () => {
-  console.log('Server is running on http://localhost:'+PORT)
-});
